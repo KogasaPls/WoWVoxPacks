64bbee23ac3ec7375527c5be365724ba1e1ca11a
Kogasa <kogasapls@gmail.com>
2025-02-27 18:14:36 -0500

    remove unused class

--- src/WoWVoxPack.AddOns.SharedMedia_Causese/CauseseSoundFile.cs
+++
@@ -1,5 +0,0 @@
-using Google.Protobuf;
-
-namespace WoWVoxPack.AddOns.SharedMedia_Causese;
-
-internal record CauseseSoundFile(string FileName, ByteString AudioContent) : AddOnSoundFile(FileName, AudioContent);

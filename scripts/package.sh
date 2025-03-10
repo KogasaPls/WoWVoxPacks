0d39285521f1e7930ce42d323d0160b0b238e5df
Kogasa <kogasapls@gmail.com>
2025-03-10 17:51:38 -0400

    fix package script

--- scripts/package.sh
+++ scripts/package.sh
@@ -12,10 +12,10 @@ cd output || exit 1
 
 while IFS= read -r -d '' file; do
   voice_name=$(basename "$(dirname "$file")")
-  addon_name=$(basename "$file")
+  addon_dir=$(basename "$file")
 
   # Trim _${voice_name} out of addon_name if it exists
-  addon_name=${addon_name%"_$voice_name"}
+  addon_name=${addon_dir%"_$voice_name"}
 
   # Trim _WoWVoxPacks out of addon_name if it exists
   addon_name=${addon_name%"_WoWVoxPacks"}
@@ -23,10 +23,10 @@ while IFS= read -r -d '' file; do
   echo "Processing directory: $file"
   echo "Creating archive for voice: $voice_name, addon: $addon_name"
 
-  cd "$file" || exit 1
+  cd "$voice_name" || exit 1
 
   archive_name="WoWVoxPacks_${voice_name}_${addon_name}_${RELEASE_TAG}.zip"
-  zip -r -q -9 "../../../dist/$archive_name" . -x "*.wav"
+  zip -r -q -9 "../../dist/$archive_name" "${addon_dir}" -x "*.wav"
 
   if [ $? -ne 0 ]; then
     echo "Failed to create archive for $file"
@@ -35,5 +35,5 @@ while IFS= read -r -d '' file; do
 
   echo "Created archive: dist/$archive_name"
 
-  cd ../.. || exit 1
+  cd .. || exit 1
 done < <(find . -mindepth 2 -maxdepth 2 -type d -print0)
